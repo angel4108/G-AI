bffc6e28be89a29caeec852ed5954706fc6756c4
zhanghy-b <zhanghy-b@glodon.com>
2025-09-04 19:13:47 +0800

    整合流程

--- python/parse_cov.py
+++ python/parse_cov.py
@@ -1,7 +1,9 @@
 import xml.etree.ElementTree as ET
 import sys
 import json  # 导入json模块用于处理JSON数据
-
+from typing import List, Dict, Any, Optional
+from pathlib import Path
+from utils import load_config
 
 def load_filter_list(filter_file):
     """加载过滤文件列表，每行一个路径"""
@@ -14,7 +16,7 @@ def load_filter_list(filter_file):
         sys.exit(1)
 
 
-def find_zero_coverage_src(xml_file, filter_list):
+def find_coverage_reach_funcs(xml_file, filter_list):
     """
     解析XML文件，查找所有fn_cov和cd_cov都为0的src元素，并返回其路径
 
@@ -45,7 +47,8 @@ def find_zero_coverage_src(xml_file, filter_list):
                 src_name = element.get("name", "")
 
                 # 检查是否两个覆盖率都为0
-                if fn_cov == "0" and cd_cov == "0":
+                # if fn_cov == "0" and cd_cov == "0":
+                if fn_cov != "0":
                     # 构建完整路径
                     full_path = (
                         f"{current_path}/{src_name}" if current_path else src_name
@@ -80,38 +83,55 @@ def find_zero_coverage_src(xml_file, filter_list):
     except Exception as e:
         print(f"解析XML文件时出错: {str(e)}", file=sys.stderr)
         return []
-
-
-def main():
-    # 命令行参数：XML文件路径、输出JSON路径、过滤文件路径
-    if len(sys.argv) != 4:
-        print(
-            f"用法: {sys.argv[0]} <xml_file_path> <output_json_path> <filter_file_path>",
-            file=sys.stderr,
-        )
-        print("过滤文件格式：每行一个需要排除的文件路径", file=sys.stderr)
-        sys.exit(1)
-
-    xml_path = sys.argv[1]
-    output_json_path = sys.argv[2]
-    filter_file_path = sys.argv[3]  # 新增：过滤文件路径
-
-    # 加载过滤列表
-    filter_list = load_filter_list(filter_file_path)
+    
+def parse_cov_filter_func(cov_config: Dict[str, Any])->List[str]:
+    filter_list = cov_config["filter_dirs"]
 
     # 遍历覆盖率为0的文件
-    zero_coverage_srcs = find_zero_coverage_src(xml_path, filter_list)
-
+    coverage_reach_funcs = find_coverage_reach_funcs(cov_config['cov_xml'], filter_list)
     # 将结果写入JSON文件
     try:
-        with open(output_json_path, "w", encoding="utf-8") as f:
-            # 写入JSON数据，使用indent参数美化输出，ensure_ascii=False确保中文正常显示
-            json.dump(zero_coverage_srcs, f, ensure_ascii=False, indent=4)
-        print(f"成功将{len(zero_coverage_srcs)}个结果写入到{output_json_path}")
+        cov_filter_ouput_file = cov_config['cov_filter_output_file']
+        if Path(cov_filter_ouput_file).parent.exists():
+            with open(cov_filter_ouput_file, "w", encoding="utf-8") as f:
+                # 写入JSON数据，使用indent参数美化输出，ensure_ascii=False确保中文正常显示
+                json.dump(coverage_reach_funcs, f, ensure_ascii=False, indent=4)
+            print(f"成功将{len(coverage_reach_funcs)}个结果写入到{cov_filter_ouput_file}")
+        return coverage_reach_funcs
     except Exception as e:
         print(f"写入JSON文件时出错: {str(e)}", file=sys.stderr)
-        sys.exit(1)
+        return []
 
+def modify_func_name(zero_cov_srcs: List) -> Optional[str]:
+    results = []
+    for src in zero_cov_srcs:
+        func_name = src.split('/')[-1]
+        if func_name.find('::') != -1:
+            args = func_name.split('::')
+            if args[-1][0] == '~':
+                results.append(args[-1])
+            elif args[-1].find(f'{args[-2]}'+'(') != -1:
+                results.append(args[-1])
+            else:
+                func_name = f'{args[-2]}::{args[-1]}'
+                if func_name.find(') const' ) != -1:
+                    func_name = func_name.replace(') const', ')')
+                results.append(func_name)
+        else:
+            results.append(func_name)
+    return results
+
+def search_cov_reachable_funcs(cov_config: Dict[str, Any]) -> List[str]:
+    coverage_reach_funcs = parse_cov_filter_func(cov_config)
+    extract_func_results = modify_func_name(coverage_reach_funcs)
+    return extract_func_results
 
 if __name__ == "__main__":
-    main()
+    # cov_config = load_config('config/cov_config.json')
+    # coverage_reach_funcs = parse_cov_filter_func(cov_config)
+
+    coverage_reach_srcs = load_config('./result/filtered_cov.json')
+    extract_func_results = modify_func_name(coverage_reach_srcs)
+    from utils import dump
+    dump(extract_func_results, Path('./result') / 'modify_cov_funcs.json')
+    
